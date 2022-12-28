18825fdd1cfe15fa26fb239b8ffcbae2c27965a5
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-12-27 21:14:54 -0500

    feat(internal): `kIsNodeError`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/internal/__tests__/k-is-node-error.spec.ts
@@ -0,0 +1,12 @@
+/**
+ * @file Unit Tests - kIsNodeError
+ * @module create-error-node/internal/tests/unit/kIsNodeError
+ */
+
+import testSubject from '../k-is-node-error'
+
+describe('unit:internal/kIsNodeError', () => {
+  it('should have description "kIsNodeError"', () => {
+    expect(testSubject.description).to.equal('kIsNodeError')
+  })
+})
---
+++ src/internal/k-is-node-error.ts
@@ -0,0 +1,13 @@
+/**
+ * @file Internal - kIsNodeError
+ * @module create-node-error/internal/kIsNodeError
+ */
+
+/**
+ * Node.js error symbol.
+ *
+ * @const {symbol} kIsNodeError
+ */
+const kIsNodeError: symbol = Symbol('kIsNodeError')
+
+export default kIsNodeError
