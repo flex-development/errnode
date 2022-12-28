14b782664771ca9d7b17fdb8fb4a78067fa0e6bb
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-12-28 05:33:56 -0500

    release: 1.0.0
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,18 +4,16 @@
 
 # 1. run tests
 # 2. pack project
-# 3. build docs
-# 4. get new package version
-# 5. get release branch name
-# 6. switch to release branch
-# 7. stage changes
-# 8. commit changes
-# 9. push release branch to origin
-# 10. cleanup
+# 3. get new package version
+# 4. get release branch name
+# 5. switch to release branch
+# 6. stage changes
+# 7. commit changes
+# 8. push release branch to origin
+# 9. cleanup
 
 yarn test:cov
 yarn pack -o %s-%v.tgz
-yarn docs:build
 VERSION=$(jq .version package.json -r)
 RELEASE_BRANCH=release/$VERSION
 git switch -c $RELEASE_BRANCH
