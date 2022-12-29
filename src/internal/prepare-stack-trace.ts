fab7e3ae01b42fc49d44e3e179a71398dda2b93b
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-12-28 20:12:09 -0500

    fix(internal): [`prepareStackTrace`] escape error message
    - prevents "Invalid regular expression" errors
    
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/internal/prepare-stack-trace.ts
+++ src/internal/prepare-stack-trace.ts
@@ -23,7 +23,19 @@ function prepareStackTrace<T extends Error = Error>(
 
   // make sure stack trace is formatted properly
   error.stack = error.stack!.replace(
-    new RegExp(`^${error.name}: ${error.message}`),
+    new RegExp(
+      `^${error.name}: ${error.message
+        /*
+         * Escape characters with special meaning, either inside or outside
+         * character sets.
+         *
+         * A simple backslash escape is used when it’s always valid; a `\xnn`
+         * escape is used when the simpler form would be disallowed by stricter
+         * unicode patterns.
+         */
+        .replace(/[$()*+.?[\\\]^{|}]/g, '\\$&')
+        .replace(/-/g, '\\x2d')}`
+    ),
     error.toString()
   )
 
