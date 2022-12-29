964c15b34452f3066722ec49c750e048d2ade699
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-12-29 01:47:51 -0500

    test(enums): [`ErrorCode`] update type test strategy
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/enums/__tests__/error-code.spec-d.ts
+++ src/enums/__tests__/error-code.spec-d.ts
@@ -7,8 +7,12 @@ import type TestSubject from '../error-code'
 
 describe('unit-d:enums/ErrorCode', () => {
   it('should have members of type Uppercase<string>', () => {
-    expectTypeOf<typeof TestSubject>()
-      .extract<Uppercase<string>>()
-      .not.toBeNever()
+    // Arrange
+    type Key = keyof typeof TestSubject
+
+    // Expect
+    expectTypeOf<Key>().toMatchTypeOf<Uppercase<string>>()
+    expectTypeOf<TestSubject>().toMatchTypeOf<Key>()
+    expectTypeOf<typeof TestSubject[Key]>().toEqualTypeOf<TestSubject>()
   })
 })
