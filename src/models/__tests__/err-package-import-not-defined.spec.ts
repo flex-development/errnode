c86086f5d50a632b8e33d674aac815070fd6a924
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-12-31 13:55:44 -0500

    feat(models): `ERR_PACKAGE_IMPORT_NOT_DEFINED`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/models/__tests__/err-package-import-not-defined.spec.ts
@@ -0,0 +1,56 @@
+/**
+ * @file Unit Tests - ERR_PACKAGE_IMPORT_NOT_DEFINED
+ * @module errnode/models/tests/unit/ERR_PACKAGE_IMPORT_NOT_DEFINED
+ */
+
+import { ErrorCode } from '#src/enums'
+import TestSubject from '../err-package-import-not-defined'
+
+describe('unit:models/ERR_PACKAGE_IMPORT_NOT_DEFINED', () => {
+  let base: string
+  let dir: string
+  let message: string
+  let specifier: string
+
+  beforeEach(() => {
+    base = import.meta.url
+    dir = process.cwd()
+    specifier = '#src'
+    message = `Package import specifier '${specifier}' is not defined`
+  })
+
+  it('should return TypeError instance', () => {
+    // Act
+    const result = new TestSubject(specifier, base)
+
+    // Expect
+    expect(result).to.be.instanceof(TypeError)
+    expect(result).to.have.property('name').equal('TypeError')
+  })
+
+  it('should set error code', () => {
+    expect(new TestSubject(specifier, base))
+      .to.have.property('code')
+      .equal(ErrorCode.ERR_PACKAGE_IMPORT_NOT_DEFINED)
+  })
+
+  it('should set error message', () => {
+    // Arrange
+    const cases: [...ConstructorParameters<typeof TestSubject>, string][] = [
+      [specifier, base, undefined, `${message} imported from ${base}`],
+      [
+        specifier,
+        base,
+        dir,
+        `${message} in package ${dir}package.json imported from ${base}`
+      ]
+    ]
+
+    // Act + Expect
+    cases.forEach(([specifier, base, dir, expected]) => {
+      expect(new TestSubject(specifier, base, dir))
+        .to.have.property('message')
+        .equal(expected)
+    })
+  })
+})
