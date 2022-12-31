9b8b7558a49ff99d1e8853f33aa9ac53c663bcde
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-12-31 13:39:44 -0500

    feat(models): `ERR_INVALID_PACKAGE_CONFIG`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/models/err-invalid-package-config.ts
@@ -0,0 +1,63 @@
+/**
+ * @file Error Models - ERR_INVALID_PACKAGE_CONFIG
+ * @module errnode/models/ERR_INVALID_PACKAGE_CONFIG
+ * @see https://github.com/nodejs/node/blob/v19.3.0/lib/internal/errors.js#L1327-L1330
+ */
+
+import { ErrorCode } from '#src/enums'
+import type { MessageFn, NodeError, NodeErrorConstructor } from '#src/types'
+import { createNodeError } from '#src/utils'
+
+/**
+ * `ERR_INVALID_PACKAGE_CONFIG` model.
+ *
+ * Thrown when a [`package.json`][1] file fails parsing.
+ *
+ * [1]: https://nodejs.org/api/packages.html#nodejs-packagejson-field-definitions
+ *
+ * @see https://nodejs.org/api/errors.html#err_invalid_package_config
+ *
+ * @class
+ *
+ * @param {string} id - Location of invalid `package.json` file
+ * @param {string?} [base=''] - Id of module being imported. May also include
+ * where module is being imported from
+ * @param {string?} [reason=''] - Reason package config is invalid
+ * @return {NodeError} `Error` instance
+ */
+const ERR_INVALID_PACKAGE_CONFIG: NodeErrorConstructor<
+  ErrorConstructor,
+  MessageFn<[string, string?, string?]>
+> = createNodeError(
+  ErrorCode.ERR_INVALID_PACKAGE_CONFIG,
+  Error,
+  /**
+   * Creates an [`ERR_INVALID_PACKAGE_CONFIG`][1] message.
+   *
+   * [1]: https://nodejs.org/api/errors.html#err_invalid_package_config
+   *
+   * @param {string} id - Location of invalid `package.json` file
+   * @param {string?} [base=''] - Id of module being imported. May also include
+   * where module is being imported from
+   * @param {string?} [reason=''] - Reason package config is invalid
+   * @return {string} Error message
+   */
+  (id: string, base: string = '', reason: string = ''): string => {
+    /**
+     * Error message.
+     *
+     * @var {string} message
+     */
+    let message: string = `Invalid package config ${id}`
+
+    // add import details
+    if (base) message += ` while importing ${base}`
+
+    // add reason package config is invalid
+    if (reason) message += `. ${reason}`
+
+    return message
+  }
+)
+
+export default ERR_INVALID_PACKAGE_CONFIG
--- src/models/index.ts
+++ src/models/index.ts
@@ -13,6 +13,7 @@ export { default as ERR_IMPORT_ASSERTION_TYPE_MISSING } from './err-import-asser
 export { default as ERR_IMPORT_ASSERTION_TYPE_UNSUPPORTED } from './err-import-assertion-type-unsupported'
 export { default as ERR_INCOMPATIBLE_OPTION_PAIR } from './err-incompatible-option-pair'
 export { default as ERR_INVALID_MODULE_SPECIFIER } from './err-invalid-module-specifier'
+export { default as ERR_INVALID_PACKAGE_CONFIG } from './err-invalid-package-config'
 export { default as ERR_METHOD_NOT_IMPLEMENTED } from './err-method-not-implemented'
 export { default as ERR_MISSING_OPTION } from './err-missing-option'
 export { default as ERR_MODULE_NOT_FOUND } from './err-module-not-found'
