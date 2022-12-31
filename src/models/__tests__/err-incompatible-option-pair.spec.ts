573fb2f00151cd1b3636b8152ddbc164b7b484a4
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-12-30 19:01:39 -0500

    feat(models): `ERR_INCOMPATIBLE_OPTION_PAIR`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/models/__tests__/err-incompatible-option-pair.spec.ts
@@ -0,0 +1,39 @@
+/**
+ * @file Unit Tests - ERR_INCOMPATIBLE_OPTION_PAIR
+ * @module errnode/models/tests/unit/ERR_INCOMPATIBLE_OPTION_PAIR
+ */
+
+import { ErrorCode } from '#src/enums'
+import type { NodeError } from '#src/types'
+import TestSubject from '../err-incompatible-option-pair'
+
+describe('unit:models/ERR_INCOMPATIBLE_OPTION_PAIR', () => {
+  let option1: string
+  let option2: string
+  let result: NodeError<TypeError>
+
+  beforeEach(() => {
+    option1 = 'options.inspectOptions.color'
+    option2 = 'colorMode'
+    result = new TestSubject(option1, option2)
+  })
+
+  it('should return TypeError instance', () => {
+    expect(result).to.have.property('name').equal('TypeError')
+    expect(result).to.be.instanceof(TypeError)
+  })
+
+  it('should set error code', () => {
+    expect(result)
+      .to.have.property('code')
+      .equal(ErrorCode.ERR_INCOMPATIBLE_OPTION_PAIR)
+  })
+
+  it('should set error message', () => {
+    // Arrange
+    const expected: string = `Option '${option1}' cannot be used in combination with option '${option2}'`
+
+    // Expect
+    expect(result).to.have.property('message').equal(expected)
+  })
+})
---
+++ src/models/err-incompatible-option-pair.ts
@@ -0,0 +1,34 @@
+/**
+ * @file Error Models - ERR_INCOMPATIBLE_OPTION_PAIR
+ * @module errnode/models/ERR_INCOMPATIBLE_OPTION_PAIR
+ * @see https://github.com/nodejs/node/blob/v19.3.0/lib/internal/errors.js#L1170-L1171
+ */
+
+import { ErrorCode } from '#src/enums'
+import type { NodeError, NodeErrorConstructor } from '#src/types'
+import { createNodeError } from '#src/utils'
+
+/**
+ * `ERR_INCOMPATIBLE_OPTION_PAIR` model.
+ *
+ * Thrown when an option pair is incompatible with each other and cannot be used
+ * at the same time.
+ *
+ * @see https://nodejs.org/api/errors.html#err_incompatible_option_pair
+ *
+ * @class
+ *
+ * @param {string} option1 - Option that cannot be ued
+ * @param {string} option2 - Option that is incompatible
+ * @return {NodeError<TypeError>} `TypeError` instance
+ */
+const ERR_INCOMPATIBLE_OPTION_PAIR: NodeErrorConstructor<
+  TypeErrorConstructor,
+  [string, string]
+> = createNodeError(
+  ErrorCode.ERR_INCOMPATIBLE_OPTION_PAIR,
+  TypeError,
+  "Option '%s' cannot be used in combination with option '%s'"
+)
+
+export default ERR_INCOMPATIBLE_OPTION_PAIR
