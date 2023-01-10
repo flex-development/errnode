bd7c87352f1b33e4cba1b38b50a3048ad4685696
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-01-09 19:44:58 -0500

    build(deps): bump @flex-development/tutils from 6.0.0-alpha.7 to 6.0.0-alpha.9
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- typings/node/loader.d.ts
+++ typings/node/loader.d.ts
@@ -1,30 +1,10 @@
-import type { Nullable } from '@flex-development/tutils'
+import type { Nullable, TypedArray } from '@flex-development/tutils'
 
 declare global {
   /**
    * Module formats.
    */
   type Format = 'builtin' | 'commonjs' | 'json' | 'module' | 'wasm'
-
-  /**
-   * Array-like view of an underlying [binary data buffer][1].
-   *
-   * [1]: https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer
-   *
-   * @see https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/TypedArray
-   */
-  type TypedArray =
-    | BigInt64Array
-    | BigUint64Array
-    | Float32Array
-    | Float64Array
-    | Int8Array
-    | Int16Array
-    | Int32Array
-    | Uint8Array
-    | Uint8ClampedArray
-    | Uint16Array
-    | Uint32Array
 
   /**
    * {@link LoadHook} context.
