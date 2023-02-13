224fd244ddf260fae7db1dbc9998ed82601541cb
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-02-12 22:03:35 -0500

    build(deps-dev): bump typescript from 5.0.0-dev.20230128 to 5.0.0-dev.20230212
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,17 +5,19 @@
 # 1. run typecheck
 # 2. run tests
 # 3. pack project
-# 4. get new package version
-# 5. get release branch name
-# 6. switch to release branch
-# 7. stage changes
-# 8. commit changes
-# 9. push release branch to origin
-# 10. cleanup
+# 4. run postbuild typecheck
+# 5. get new package version
+# 6. get release branch name
+# 7. switch to release branch
+# 8. stage changes
+# 9. commit changes
+# 10. push release branch to origin
+# 11. cleanup
 
 yarn typecheck
 yarn test:cov
 yarn pack -o %s-%v.tgz
+yarn check:types:build
 VERSION=$(jq .version package.json -r)
 RELEASE_BRANCH=release/$VERSION
 git switch -c $RELEASE_BRANCH
